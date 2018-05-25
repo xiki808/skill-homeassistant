ae6e17e7e165ef8c3543e9e8ec2ee8a25f22cce5
btotharye <brianhh1230@gmail.com>
2018-05-25 18:15:12 -0400

    updating for light test

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,13 @@
 from test.integrationtests.skills.skill_tester import SkillTest
 
 import mock
+
+
+def test_runner(skill, example, emitter, loader):
+
+    ha_data = {'entity_id': 'light.kitchen_lights'}
+
+    s = [s for s in loader.skills if s and s.root_dir == skill]
+    s[0].homeassistant = mock.MagicMock()
+    s[0].homeassistant.ha.execute_service("homeassistant", "turn_on", ha_data)
+    return SkillTest(skill, example, emitter).run(loader)
