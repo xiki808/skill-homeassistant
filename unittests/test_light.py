5f5dd689a7474266bd303701bae74a2f8959fb02
btotharye <brianhh1230@gmail.com>
2018-05-27 18:13:02 -0400

    refactored to one function to toggle lights

--- unittests/test_light.py
+++ unittests/test_light.py
@@ -12,15 +12,26 @@ class TestHaClient(TestCase):
         if entity['dev_name'] == 'Kitchen Lights':
             self.assertTrue(True)
 
-    def test_turn_on_lights(self):
+    def test_toggle_lights(self):
         ha = HomeAssistantClient('167.99.144.205', 'password', 'ssl' == 'false', 'verify' == 'false')
         entity = ha.find_entity(kitchen_light['dev_name'], 'light')
         ha_data = {'entity_id': entity['id']}
-        action = 'on'
-        r = ha.execute_service("homeassistant", "turn_%s" % action,
+        state = entity['state']
+        if state == 'on':
+            r = ha.execute_service("homeassistant", "turn_off",
                            ha_data)
-        if r.status_code == 200:
-            self.assertTrue(True)
+            if r.status_code == 200:
+                entity = ha.find_entity(kitchen_light['dev_name'], 'light')
+                if entity['state'] == 'off':
+                    self.assertTrue(True)
+        else:
+            r = ha.execute_service("homeassistant", "turn_on",
+                                   ha_data)
+            if r.status_code == 200:
+                if entity['state'] == 'on':
+                    self.assertTrue(True)
+
+
 
 
 
