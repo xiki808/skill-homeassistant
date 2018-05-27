02e98ca973a9305ef8028277076d46b0586ab69c
btotharye <brianhh1230@gmail.com>
2018-05-27 18:25:20 -0400

    add test for light attr

--- unittests/test_light.py
+++ unittests/test_light.py
@@ -1,10 +1,18 @@
 from unittest import TestCase
 from ha_client import HomeAssistantClient
+import unittest
 
 kitchen_light = {'state': 'off', 'id': '1', 'dev_name': 'kitchen'}
 
 
 class TestHaClient(TestCase):
+
+    def test_light_attr(self):
+        ha = HomeAssistantClient('167.99.144.205', 'password', 'ssl' == 'false', 'verify' == 'false')
+        entity = ha.find_entity(kitchen_light['dev_name'], 'light')
+        light_attrs = ha.find_entity_attr(entity['id'])
+        if light_attrs[1] == 'Kitchen Lights':
+            self.assertTrue(True)
 
     def test_find_entity(self):
         ha = HomeAssistantClient('167.99.144.205', 'password', 'ssl' == 'false', 'verify' == 'false')
@@ -30,9 +38,6 @@ class TestHaClient(TestCase):
             if r.status_code == 200:
                 if entity['state'] == 'on':
                     self.assertTrue(True)
-
-
-
 
 
 if __name__ == '__main__':
