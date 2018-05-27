b32ead44137bb51108ed20f5222da04d98563ec2
btotharye <brianhh1230@gmail.com>
2018-05-27 12:10:09 -0400

    setup tests and moved HA Client into seperate module to clean up code

---
+++ ha_client.py
@@ -0,0 +1,149 @@
+from os.path import dirname, join
+from requests import get, post
+from requests.exceptions import ConnectionError
+from fuzzywuzzy import fuzz
+import json
+
+__author__ = 'btotharye'
+
+# Timeout time for HA requests
+TIMEOUT = 10
+
+
+class HomeAssistantClient(object):
+    def __init__(self, host, password, portnum, ssl=False, verify=True):
+        self.ssl = ssl
+        self.verify = verify
+        if portnum is None or portnum == 0:
+            portnum = 8123
+        if self.ssl:
+            self.url = "https://%s:%d" % (host, portnum)
+        else:
+            self.url = "http://%s:%d" % (host, portnum)
+        self.headers = {
+            'x-ha-access': password,
+            'Content-Type': 'application/json'
+        }
+
+    def find_entity(self, entity, types):
+        if self.ssl:
+            req = get("%s/api/states" %
+                      self.url, headers=self.headers,
+                      verify=self.verify, timeout=TIMEOUT)
+        else:
+            req = get("%s/api/states" % self.url, headers=self.headers,
+                      timeout=TIMEOUT)
+
+        if req.status_code == 200:
+            # require a score above 50%
+            best_score = 50
+            best_entity = None
+            for state in req.json():
+                try:
+                    if state['entity_id'].split(".")[0] in types:
+                        # something like temperature outside
+                        # should score on "outside temperature sensor"
+                        # and repetitions should not count on my behalf
+                        score = fuzz.token_sort_ratio(
+                            entity,
+                            state['attributes']['friendly_name'].lower())
+                        if score > best_score:
+                            best_score = score
+                            best_entity = {
+                                "id": state['entity_id'],
+                                "dev_name": state['attributes']
+                                ['friendly_name'],
+                                "state": state['state']}
+                        score = fuzz.token_sort_ratio(
+                            entity,
+                            state['entity_id'].lower())
+                        if score > best_score:
+                            best_score = score
+                            best_entity = {
+                                "id": state['entity_id'],
+                                "dev_name": state['attributes']
+                                ['friendly_name'],
+                                "state": state['state']}
+                except KeyError:
+                    pass
+            return best_entity
+    #
+    # checking the entity attributes to be used in the response dialog.
+    #
+
+    def find_entity_attr(self, entity):
+        if self.ssl:
+            req = get("%s/api/states" %
+                      self.url, headers=self.headers, verify=self.verify,
+                      timeout=TIMEOUT)
+        else:
+            req = get("%s/api/states" % self.url, headers=self.headers,
+                      timeout=TIMEOUT)
+
+        if req.status_code == 200:
+            for attr in req.json():
+                if attr['entity_id'] == entity:
+                    entity_attrs = attr['attributes']
+                    try:
+                        if attr['entity_id'].startswith('light.'):
+                            # Not all lamps do have a color
+                            unit_measur = entity_attrs['brightness']
+                        else:
+                            unit_measur = entity_attrs['unit_of_measurement']
+                    except KeyError:
+                        unit_measur = None
+                    # IDEA: return the color if available
+                    # TODO: change to return the whole attr dictionary =>
+                    # free use within handle methods
+                    sensor_name = entity_attrs['friendly_name']
+                    sensor_state = attr['state']
+                    return unit_measur, sensor_name, sensor_state
+        return None
+
+    def execute_service(self, domain, service, data):
+        if self.ssl:
+            post("%s/api/services/%s/%s" % (self.url, domain, service),
+                 headers=self.headers, data=json.dumps(data),
+                 verify=self.verify, timeout=TIMEOUT)
+        else:
+            post("%s/api/services/%s/%s" % (self.url, domain, service),
+                 headers=self.headers, data=json.dumps(data), timeout=TIMEOUT)
+
+    def find_component(self, component):
+        """Check if a component is loaded at the HA-Server"""
+        if self.ssl:
+            req = get("%s/api/components" %
+                      self.url, headers=self.headers, verify=self.verify,
+                      timeout=TIMEOUT)
+        else:
+            req = get("%s/api/components" % self.url, headers=self.headers,
+                      timeout=TIMEOUT)
+
+        if req.status_code == 200:
+            return component in req.json()
+
+    def engage_conversation(self, utterance):
+        """Engage the conversation component at the Home Assistant server
+        Attributes:
+            utterance    raw text message to be processed
+        Return:
+            Dict answer by Home Assistant server
+            { 'speech': textual answer,
+              'extra_data': ...}
+        """
+        data = {
+            "text": utterance
+        }
+        if self.ssl:
+            return post("%s/api/conversation/process" % (self.url),
+                        headers=self.headers,
+                        data=json.dumps(data),
+                        verify=self.verify,
+                        timeout=TIMEOUT
+                        ).json()['speech']['plain']
+        else:
+            return post("%s/api/conversation/process" % (self.url),
+                        headers=self.headers,
+                        data=json.dumps(data),
+                        timeout=TIMEOUT
+                        ).json()['speech']['plain']
---
+++ test/unittests/test_light.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from ha_client import HomeAssistantClient
+
+kitchen_light = {'state': 'off', 'id': '1', 'dev_name': 'kitchen'}
+
+class TestHaClient(TestCase):
+
+    def test_find_entity(self):
+        ha = HomeAssistantClient('167.99.144.205', 'password', 'ssl' == 'false', 'verify' == 'false')
+        entity = ha.find_entity(kitchen_light['dev_name'], 'light')
+        if entity['dev_name'] == 'Kitchen Lights':
+            self.assertTrue(True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
