07354b8c6e29863b1ced2a591070540184f52e0a
btotharye <brianhh1230@gmail.com>
2018-05-28 00:29:27 -0400

    testing for ssl and non ssl

--- unittests/test_haclient.py
+++ unittests/test_haclient.py
@@ -18,7 +18,7 @@ json_data = {'attributes': {'friendly_name': 'Kitchen Lights',
 class TestHaClient(TestCase):
 
     @mock.patch('requests.get')
-    def test_light(self, mock_get):
+    def test_light_ssl(self, mock_get):
         portnum = None
         ha = HomeAssistantClient(host='192.168.0.1', password='password', portnum=portnum, ssl=True)
         mock_resp = mock.Mock()
@@ -32,6 +32,25 @@ class TestHaClient(TestCase):
             print(entity)
             self.assertTrue(True)
         self.assertEqual(ha.portnum, 8123)
+        self.assertEqual(ha.url, 'https://192.168.0.1:8123')
+
+    @mock.patch('requests.get')
+    def test_light_nossl(self, mock_get):
+        portnum = None
+        ha = HomeAssistantClient(host='192.168.0.1', password='password', portnum=portnum, ssl=False)
+        mock_resp = mock.Mock()
+        mock_resp.status_code = 200
+        mock_resp.json = mock.Mock(return_value=json_data)
+        print(mock_resp.json())
+        self.assertEqual(mock_resp.json(), json_data)
+        ha.find_entity = mock.MagicMock(name='find_entity')
+        entity = ha.find_entity(kitchen_light['dev_name'], 'light')
+        if entity['dev_name'] == 'Kitchen Lights':
+            print(entity)
+            self.assertTrue(True)
+        self.assertEqual(ha.portnum, 8123)
+        self.assertEqual(ha.url, 'http://192.168.0.1:8123')
+
 
     def test_toggle_lights(self):
         ha = HomeAssistantClient(host='192.168.0.1', password='password', portnum=8123, ssl=True)
