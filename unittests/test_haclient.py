d19b18ed6282519e06a9d693de05a3a085241e58
btotharye <brianhh1230@gmail.com>
2018-05-28 00:51:03 -0400

    adding more tests

--- unittests/test_haclient.py
+++ unittests/test_haclient.py
@@ -14,6 +14,9 @@ json_data = {'attributes': {'friendly_name': 'Kitchen Lights',
  'entity_id': 'light.kitchen_lights',
  'state': 'off'}
 
+attr_resp = {
+            "id": '1',
+            "dev_name": {'attributes': {'friendly_name': 'Kitchen Lights', 'max_mireds': 500, 'min_mireds': 153, 'supported_features': 151}, 'entity_id': 'light.kitchen_lights', 'state': 'off'}}
 
 class TestHaClient(TestCase):
 
@@ -46,16 +49,20 @@ class TestHaClient(TestCase):
         ha.find_entity = mock.MagicMock(name='find_entity')
         entity = ha.find_entity(kitchen_light['dev_name'], 'light')
         if entity['dev_name'] == 'Kitchen Lights':
-            print(entity)
             self.assertTrue(True)
         self.assertEqual(ha.portnum, 8123)
         self.assertEqual(ha.url, 'http://192.168.0.1:8123')
 
-
-    def test_toggle_lights(self):
+    @mock.patch('ha_client.HomeAssistantClient.find_entity')
+    def test_toggle_lights(self, mock_get):
         ha = HomeAssistantClient(host='192.168.0.1', password='password', portnum=8123, ssl=True)
         ha.find_entity = mock.MagicMock()
         entity = ha.find_entity(kitchen_light['dev_name'], 'light')
+        print(mock_get)
+        mock_get.entity = {
+                "id": '1',
+                "dev_name": {'attributes': {'friendly_name': 'Kitchen Lights', 'max_mireds': 500, 'min_mireds': 153, 'supported_features': 151}, 'entity_id': 'light.kitchen_lights', 'state': 'off'}}
+        self.assertEqual(mock_get.entity, attr_resp)
         ha_data = {'entity_id': entity['id']}
         state = entity['state']
         if state == 'on':
