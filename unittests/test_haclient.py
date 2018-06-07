69b7f66c66e97213432f534bf60cab468d8d2500
btotharye <brianhh1230@gmail.com>
2018-06-07 07:23:22 -0400

    updating tests

--- unittests/test_haclient.py
+++ unittests/test_haclient.py
@@ -25,6 +25,13 @@ headers = {
 
 class TestHaClient(TestCase):
 
+    @mock.patch('requests.get')
+    def test_connect(self, mock_get):
+        portnum = None
+        ssl = True
+        ha = HomeAssistantClient(host='192.168.0.1', password='password', portnum=portnum, ssl=ssl)
+        self.assertEqual(ha.portnum, 8123)
+
     @mock.patch('ha_client.HomeAssistantClient.find_entity')
     def test_toggle_lights(self, mock_get):
         ha = HomeAssistantClient(host='192.168.0.1', password='password', portnum=8123, ssl=True)
