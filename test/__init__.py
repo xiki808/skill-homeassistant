727ebf4fc0bc639c2d8b2979f1b8a0dedb25a336
btotharye <brianhh1230@gmail.com>
2018-07-04 21:52:38 -0400

    updating tests

--- test/__init__.py
+++ test/__init__.py
@@ -93,6 +93,10 @@ def test_runner(skill, example, emitter, loader):
         s[0].ha.find_entity.return_value = kitchen_light_off
         s[0].ha.find_entity_attr.return_value = kitchen_light_attr
 
+    if example.endswith('019.IncreaseLightBright.intent.json'):
+        s[0].ha = mock.MagicMock()
+        s[0].ha.find_entity.return_value = kitchen_light_on
+        s[0].ha.find_entity_attr.return_value = kitchen_light_attr
 
 
     return SkillTest(skill, example, emitter).run(loader)
