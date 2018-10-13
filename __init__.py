1f06fac41825b00a200aaba7cc8d0f6b01caf638
btotharye <brianhh1230@gmail.com>
2018-10-13 09:05:15 -0400

    reverting a few changes

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ class HomeAssistantSkill(FallbackSkill):
 
     def __init__(self):
         MycroftSkill.__init__(self)
-        super(HomeAssistantSkill, self).__init__(name="HomeAssistantSkill")
+        super().__init__(name="HomeAssistantSkill")
         self.ha = None
         self.enable_fallback = False
 
@@ -68,7 +68,7 @@ class HomeAssistantSkill(FallbackSkill):
         self.language = self.config_core.get('lang')
         self.load_vocab_files(join(dirname(__file__), 'vocab', self.lang))
         self.load_regex_files(join(dirname(__file__), 'regex', self.lang))
-        self.handle_switch_intent()
+        self.__build_switch_intent()
         self.__build_light_adjust_intent()
         self.__build_automation_intent()
         self.__build_sensor_intent()
@@ -92,8 +92,6 @@ class HomeAssistantSkill(FallbackSkill):
         # Otherwise new settings will not be regarded
         self._force_setup()
 
-    @intent_handler(IntentBuilder("").require(
-        "SwitchActionKeyword").require("Action").require("Entity"))
     def handle_switch_intent(self):
         intent = IntentBuilder("switchIntent").require(
             "SwitchActionKeyword").require("Action").require("Entity").build()
