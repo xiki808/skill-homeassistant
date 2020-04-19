7efa3d93b73ac5d91d275b8833c07cbfbf193821
roberto <roberto@pfire.cloud>
2020-04-19 16:02:38 +0200

    Try the launch speak dialog

--- __init__.py
+++ __init__.py
@@ -260,7 +260,7 @@ class HomeAssistantSkill(FallbackSkill):
         entity = message.data["entity"]
         ha_data = {'name': entity}
         self.ha.execute_service("shopping_list", "add_item", ha_data)
-        self.speak_dialog("homeassistant.shopping.list.dialog")
+        self.speak_dialog("homeassistant.shopping.list")
         return
 
     def handle_light_adjust_intent(self, message):
