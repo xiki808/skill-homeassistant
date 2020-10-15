5dd476843ef3ab62bcdbd3a132801da221df9435
Antonín Skala <skala.antonin@gmail.com>
2020-10-15 19:47:12 +0200

    Feature: Entity availability check
    - Added routine for entity availability check
    - Adress PR #38

--- __init__.py
+++ __init__.py
@@ -70,7 +70,7 @@ class HomeAssistantSkill(FallbackSkill):
         self.load_regex_files(join(dirname(__file__), 'regex', self.lang))
         self.__build_automation_intent()
         self.__build_tracker_intent()
-        
+
         # Needs higher priority than general fallback skills
         self.register_fallback(self.handle_fallback, 2)
         # Check and then monitor for credential changes
@@ -110,6 +110,15 @@ class HomeAssistantSkill(FallbackSkill):
             self.speak_dialog('homeassistant.device.unknown', data={
                               "dev_name": entity})
         return ha_entity
+
+    # Routine for entiti availibility check
+    def _check_availability(self, ha_entity):
+        self.log.debug(ha_entity['state'])
+        if ha_entity['state'] == 'unavailable':
+            self.speak_dialog('homeassistant.device.unavailable', data={
+                            "dev_name": ha_entity['dev_name']})
+            return False
+        return True
 
     # Calls passed method and catches often occurring exceptions
     def _handle_client_exception(self, callback, *args, **kwargs):
@@ -211,8 +220,11 @@ class HomeAssistantSkill(FallbackSkill):
                 'climate'
             ]
         )
-        if not ha_entity:
+
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
+
         LOGGER.debug("Entity State: %s" % ha_entity['state'])
         ha_data = {'entity_id': ha_entity['id']}
 
@@ -255,8 +267,10 @@ class HomeAssistantSkill(FallbackSkill):
         LOGGER.debug("Brightness Percent: %s" % brightness_percentage)
 
         ha_entity = self._find_entity(entity, ['group', 'light'])
-        if not ha_entity:
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
+
         ha_data = {'entity_id': ha_entity['id']}
 
         # IDEA: set context for 'turn it off again' or similar
@@ -296,7 +310,8 @@ class HomeAssistantSkill(FallbackSkill):
         max_brightness = 255
 
         ha_entity = self._find_entity(entity, ['group', 'light'])
-        if not ha_entity:
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
         ha_data = {'entity_id': ha_entity['id']}
         # IDEA: set context for 'turn it off again' or similar
@@ -357,7 +372,8 @@ class HomeAssistantSkill(FallbackSkill):
             ['automation', 'scene', 'script']
         )
 
-        if not ha_entity:
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
 
         ha_data = {'entity_id': ha_entity['id']}
@@ -386,7 +402,8 @@ class HomeAssistantSkill(FallbackSkill):
         LOGGER.debug("Entity: %s" % entity)
 
         ha_entity = self._find_entity(entity, ['sensor', 'switch'])
-        if not ha_entity:
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
 
         entity = ha_entity['id']
@@ -442,7 +459,8 @@ class HomeAssistantSkill(FallbackSkill):
         LOGGER.debug("Entity: %s" % entity)
 
         ha_entity = self._find_entity(entity, ['device_tracker'])
-        if not ha_entity:
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
 
         # IDEA: set context for 'locate it again' or similar
@@ -464,7 +482,8 @@ class HomeAssistantSkill(FallbackSkill):
         LOGGER.debug("Temperature: %s" % temperature)
 
         ha_entity = self._find_entity(entity, ['climate'])
-        if not ha_entity:
+        # Exit if entiti not found or is unavailabe
+        if not ha_entity or not self._check_availability(ha_entity):
             return
 
         climate_data = {
